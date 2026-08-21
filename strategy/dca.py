0bd82f736ec42f0fef1b6e52b0704bd66e3b4ffe
Mika Novakovic <108232085+big-nuggz@users.noreply.github.com>
2026-08-21 14:39:06 +0900

    added dollar cost average strategy

---
+++ strategy/dca.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from strategy.strategy import Strategy
+
+
+class DCA(Strategy):
+    # simple DCA (dollar cost average)
+    # invests monthly regardless of the market condition
+    def __init__(self, budget, years: int):
+        super().__init__(budget)
+
+        self.years = years # first years to split investment
+        self.remaining = budget
+        self.contribution = budget / years / 12
+        self.shares = []
+        self.final_price = 0.0
+
+    def __call__(
+            self, 
+            date: datetime, 
+            open_: float, 
+            high: float, 
+            low: float, 
+            close: float) -> None:
+        self.final_price = close
+
+        # buy at the start of month
+        if date.day != 1:
+            return
+
+        # only buy if budget is available
+        if self.remaining <= 0.0:
+            return
+
+        # invest everything if remaining balance is lower than contribution value
+        if self.remaining < self.contribution:
+            self.shares += [self.remaining / close]
+            self.remaining = 0.0
+            return
+
+        self.shares += [self.contribution / close]
+        self.remaining -= self.contribution
+
+    def get_results(self):
+        # calculate the average share price and number of total shares
+        total_shares = sum(self.shares)
+
+        portfolio_value = self.final_price * total_shares
+        final_return = (portfolio_value - self.budget) / self.budget
+
+        return portfolio_value, final_return
+        
--- test.py
+++ test.py
@@ -3,12 +3,13 @@ from dateutil.relativedelta import relativedelta
 
 from data import load_sp500
 from strategy.buy_and_hold import BuyAndHold
+from strategy.dca import DCA
 
 
 # parameters
-budget = 1000 # dollars maybe
-starting_date = datetime(year=2007, month=1, day=1, tzinfo=UTC)
-testing_period = 5 # years
+budget = 10000 # dollars maybe
+starting_date = datetime(year=1990, month=1, day=1, tzinfo=UTC)
+testing_period = 10 # years
 
 data = load_sp500()
 
@@ -17,14 +18,21 @@ sliced_data = data.loc[starting_date: end_date]
 
 # initialize the strategies
 benchmark = BuyAndHold(budget)
+dca = DCA(budget, years=5)
 
 for row in sliced_data.itertuples():
     date, open_, high, low, close = row[:5]
     benchmark(date, open_, high, low, close)
+    dca(date, open_, high, low, close)
 
 benchmark_result = benchmark.get_results()
+dca_result = dca.get_results()
 
 print(f'initial investment ${budget:.2f} with {testing_period} year testing period')
 print('-' * 40)
 print('benchmark (lump sum buy and hold)')
 print(f'portfolio value: ${benchmark_result[0]:.2f}, return: {benchmark_result[1] * 100:.2f}%, CAGR: {((benchmark_result[1] + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+
+print('-' * 40)
+print('DCA')
+print(f'portfolio value: ${dca_result[0]:.2f}, return: {dca_result[1] * 100:.2f}%, CAGR: {((dca_result[1] + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
