eee6a7b75b76b27eab24681eb8f716450def4ada
Mika Novakovic <108232085+big-nuggz@users.noreply.github.com>
2026-08-21 15:54:41 +0900

    added multi year testing

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from datetime import datetime, UTC
 from dateutil.relativedelta import relativedelta
 
+import numpy as np
+
 from data import load_sp500
 from strategy.buy_and_hold import BuyAndHold
 from strategy.dca import DCA
@@ -8,31 +10,42 @@ from strategy.dca import DCA
 
 # parameters
 budget = 10000 # dollars maybe
-starting_date = datetime(year=1990, month=1, day=1, tzinfo=UTC)
+year_start = 1930
+year_end = 2010 # this + testing period is the final year in dataset
 testing_period = 10 # years
 
 data = load_sp500()
 
-end_date = starting_date + relativedelta(years=testing_period)
-sliced_data = data.loc[starting_date: end_date]
+slices = []
+for year in range(year_start, year_end + 1):
+    starting_date = datetime(year=year, month=1, day=1, tzinfo=UTC)
+    end_date = starting_date + relativedelta(years=testing_period)
+    slices += [data.loc[starting_date: end_date]]
 
-# initialize the strategies
-benchmark = BuyAndHold(budget)
-dca = DCA(budget, years=5)
+benchmark_results = []
+dca_results = []
+for sliced_data in slices:
+    # initialize the strategies
+    benchmark = BuyAndHold(budget)
+    dca = DCA(budget, years=5)
 
-for row in sliced_data.itertuples():
-    date, open_, high, low, close = row[:5]
-    benchmark(date, open_, high, low, close)
-    dca(date, open_, high, low, close)
+    for row in sliced_data.itertuples():
+        date, open_, high, low, close = row[:5]
+        benchmark(date, open_, high, low, close)
+        dca(date, open_, high, low, close)
 
-benchmark_result = benchmark.get_results()
-dca_result = dca.get_results()
+    benchmark_results += [benchmark.get_results()[1]]
+    dca_results += [dca.get_results()[1]]
+
+benchmark_return = np.mean(benchmark_results)
+dca_return = np.mean(dca_results)
 
 print(f'initial investment ${budget:.2f} with {testing_period} year testing period')
+
 print('-' * 40)
 print('benchmark (lump sum buy and hold)')
-print(f'portfolio value: ${benchmark_result[0]:.2f}, return: {benchmark_result[1] * 100:.2f}%, CAGR: {((benchmark_result[1] + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+print(f'mean return: {benchmark_return * 100:.2f}%, mean CAGR: {((benchmark_return + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
 
 print('-' * 40)
 print('DCA')
-print(f'portfolio value: ${dca_result[0]:.2f}, return: {dca_result[1] * 100:.2f}%, CAGR: {((dca_result[1] + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+print(f'mean return: {dca_return * 100:.2f}%, mean CAGR: {((dca_return + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
