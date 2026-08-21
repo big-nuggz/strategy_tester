373d074094bd453edd2d6d115fff6e1f24106afe
Mika Novakovic <108232085+big-nuggz@users.noreply.github.com>
2026-08-21 19:15:58 +0900

    added additional statistics

--- test.py
+++ test.py
@@ -10,55 +10,63 @@ from strategy.dca import DCA
 from strategy.timed_dca import TimedDCA
 
 
-# parameters
-budget = 10000 # dollars maybe
-# year_start = 1930
-year_start = 1989
-year_end = 2015 # this + testing period is the final year in dataset
-testing_period = 10 # years
+def print_result(results: list, period: int, prefix=None) -> None:
+    if prefix:
+        print(prefix)
 
-# data = load_sp500(PATH_SP500)
-data = load_sp500(PATH_SP500_TR)
+    mean_return = np.mean(results)
+    low = np.min(results)
+    high = np.max(results)
+    std = np.std(results)
+    cagr = (mean_return + 1) ** (1 / period) - 1
+    print(f'mean return: {mean_return * 100:.2f}%, mean CAGR: {cagr * 100:.2f}%')
+    print(f'low: {low * 100:.2f}%, high: {high * 100:.2f}%, std: {std * 100:.2f}%')
 
-slices = []
-for year in range(year_start, year_end + 1):
-    starting_date = datetime(year=year, month=1, day=1, tzinfo=UTC)
-    end_date = starting_date + relativedelta(years=testing_period)
-    slices += [data.loc[starting_date: end_date]]
+    return
 
-benchmark_results = []
-dca_results = []
-timed_dca_results = []
-for sliced_data in slices:
-    # initialize the strategies
-    benchmark = BuyAndHold(budget)
-    dca = DCA(budget, years=5)
-    timed_dca = TimedDCA(budget, years=5, dca_portion=0.8, threshold=0.99)
+if __name__ == '__main__':
+    # parameters
+    budget = 10000 # dollars maybe
+    # year_start = 1930
+    year_start = 1989
+    year_end = 2015 # this + testing period is the final year in dataset
+    testing_period = 10 # years
 
-    for row in sliced_data.itertuples():
-        date, open_, high, low, close = row[:5]
-        benchmark(date, open_, high, low, close)
-        dca(date, open_, high, low, close)
-        timed_dca(date, open_, high, low, close)
+    # data = load_sp500(PATH_SP500)
+    data = load_sp500(PATH_SP500_TR)
 
-    benchmark_results += [benchmark.get_results()[1]]
-    dca_results += [dca.get_results()[1]]
-    timed_dca_results += [timed_dca.get_results()[1]]
+    slices = []
+    for year in range(year_start, year_end + 1):
+        starting_date = datetime(year=year, month=1, day=1, tzinfo=UTC)
+        end_date = starting_date + relativedelta(years=testing_period)
+        slices += [data.loc[starting_date: end_date]]
 
-benchmark_return = np.mean(benchmark_results)
-dca_return = np.mean(dca_results)
-timed_dca_return = np.mean(timed_dca_results)
+    benchmark_results = []
+    dca_results = []
+    timed_dca_results = []
+    for sliced_data in slices:
+        # initialize the strategies
+        benchmark = BuyAndHold(budget)
+        dca = DCA(budget, years=5)
+        timed_dca = TimedDCA(budget, years=5, dca_portion=0.8, threshold=0.98)
 
-print(f'{testing_period} year testing period, number of samples = {len(slices)}')
+        for row in sliced_data.itertuples():
+            date, open_, high, low, close = row[:5]
+            benchmark(date, open_, high, low, close)
+            dca(date, open_, high, low, close)
+            timed_dca(date, open_, high, low, close)
 
-print('-' * 40)
-print('benchmark 1 (lump sum buy and hold)')
-print(f'mean return: {benchmark_return * 100:.2f}%, mean CAGR: {((benchmark_return + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+        benchmark_results += [benchmark.get_results()[1]]
+        dca_results += [dca.get_results()[1]]
+        timed_dca_results += [timed_dca.get_results()[1]]
 
-print('-' * 40)
-print('benchmark 2 (DCA, invested monthly in the first 5 years)')
-print(f'mean return: {dca_return * 100:.2f}%, mean CAGR: {((dca_return + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+    print(f'{testing_period} year testing period, number of samples = {len(slices)}')
 
-print('-' * 40)
-print('timed DCA')
-print(f'mean return: {timed_dca_return * 100:.2f}%, mean CAGR: {((timed_dca_return + 1) ** (1 / testing_period) - 1) * 100:.2f}%')
+    print('-' * 40)
+    print_result(benchmark_results, testing_period, 'benchmark 1 (lump sum buy and hold)')
+
+    print('-' * 40)
+    print_result(dca_results, testing_period, 'benchmark 2 (DCA, invested monthly in the first 5 years)')
+
+    print('-' * 40)
+    print_result(timed_dca_results, testing_period, 'timed DCA')
